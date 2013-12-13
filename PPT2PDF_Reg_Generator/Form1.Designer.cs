f7ee3b702fc640fab5c42267a366fbf95a71ba86
ningboliuwei <liuwei@nbu.edu.cn>
2013-12-13 22:50:05 +0800

    一次次

--- PPT2PDF_Reg_Generator/Form1.Designer.cs
+++ PPT2PDF_Reg_Generator/Form1.Designer.cs
@@ -28,22 +28,24 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
+			this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBox1.Location = new System.Drawing.Point(56, 6);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(429, 22);
+			this.textBox1.Size = new System.Drawing.Size(429, 23);
 			this.textBox1.TabIndex = 0;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(38, 15);
@@ -52,12 +54,17 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(15, 50);
+			this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(12, 58);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.Size = new System.Drawing.Size(154, 49);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
 			// Form1
 			// 
@@ -67,6 +74,7 @@
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
+			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -76,10 +84,10 @@
 
 		#endregion
 
-		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 	}
 }
 
