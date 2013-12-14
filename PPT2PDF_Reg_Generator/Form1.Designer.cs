a7d3f1347f6495db344e5503fc744d3dbf6d1081
ningboliuwei <liuwei@nbu.edu.cn>
2013-12-14 23:14:36 +0800

    设计了界面

--- PPT2PDF_Reg_Generator/Form1.Designer.cs
+++ PPT2PDF_Reg_Generator/Form1.Designer.cs
@@ -30,16 +30,17 @@
 		{
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
+			this.btnCreate = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.btnOpenFileDialog = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
 			this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.textBox1.Location = new System.Drawing.Point(56, 6);
+			this.textBox1.Location = new System.Drawing.Point(111, 6);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(429, 23);
+			this.textBox1.Size = new System.Drawing.Size(358, 23);
 			this.textBox1.TabIndex = 0;
 			// 
 			// label1
@@ -48,35 +49,46 @@
 			this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(38, 15);
+			this.label1.Size = new System.Drawing.Size(98, 15);
 			this.label1.TabIndex = 1;
-			this.label1.Text = "label1";
+			this.label1.Text = "脚本文件路径：";
 			// 
-			// button1
+			// btnCreate
 			// 
-			this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(12, 58);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(154, 49);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCreate.Location = new System.Drawing.Point(15, 35);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(538, 49);
+			this.btnCreate.TabIndex = 2;
+			this.btnCreate.Text = "创建注册（.reg）文件(&C)";
+			this.btnCreate.UseVisualStyleBackColor = true;
 			// 
 			// openFileDialog1
 			// 
 			this.openFileDialog1.FileName = "openFileDialog1";
 			// 
+			// btnOpenFileDialog
+			// 
+			this.btnOpenFileDialog.Location = new System.Drawing.Point(475, 4);
+			this.btnOpenFileDialog.Name = "btnOpenFileDialog";
+			this.btnOpenFileDialog.Size = new System.Drawing.Size(78, 24);
+			this.btnOpenFileDialog.TabIndex = 3;
+			this.btnOpenFileDialog.Text = "打开(&O)";
+			this.btnOpenFileDialog.UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
+			this.AcceptButton = this.btnCreate;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(497, 272);
-			this.Controls.Add(this.button1);
+			this.ClientSize = new System.Drawing.Size(565, 95);
+			this.Controls.Add(this.btnOpenFileDialog);
+			this.Controls.Add(this.btnCreate);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
 			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "PPT2PDF脚本注册文件生成器";
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -86,8 +98,9 @@
 
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button btnCreate;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button btnOpenFileDialog;
 	}
 }
 
